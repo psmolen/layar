60b1822ce651195b9fd9e4ef9326e02ba44e5918
pat <pat@blueye.com>
2015-07-13 17:15:30 -0500

    Initial Commit

---
+++ layar/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  layar
+//
+//  Created by Patrick Smolen on 7/13/15
+//  Copyright (c) 2015 solstice. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
